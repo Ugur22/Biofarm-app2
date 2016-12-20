9423b44933c20100224d8a5beb89b01ffb823e69
dvschaik <jsonschaik@gmail.com>
2016-12-20 12:43:37 +0100

    Responsive 1.0

--- client/trees/Trees.js
+++ client/trees/Trees.js
@@ -23,12 +23,14 @@ function randRange(minNum, maxNum) {
     return (Math.floor(Math.random() * (maxNum - minNum + 1)) + minNum);
 }
 
+var fullScreenWidth = screen.width;
+var fullScreenHeight = screen.availHeight;
 
 // function to generate drops
 function createRain() {
     for (var i = 1; i < nbDrop; i++) {
-        var dropLeft = randRange(0, 1600);
-        var dropTop = randRange(-1000, 1400);
+        var dropLeft = randRange(0, fullScreenWidth);
+        var dropTop = randRange(-1000, fullScreenHeight);
         $('.rain').append('<div class="drop" id="drop' + i + '"></div>');
         $('#drop' + i).css('left', dropLeft);
         $('#drop' + i).css('top', dropTop);
